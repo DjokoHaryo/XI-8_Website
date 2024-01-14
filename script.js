00d9917f7e257878217560d519289becf49179b3
Revanalkamar <129585890+revanalkamar@users.noreply.github.com>
2024-01-14 21:05:18 +0700

    update js,css

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Toggle class active
-const navbarNav = document.querySelector ('.navbar');
-
-
 // ==============navbar hide and show==============
 
 const barbtn = document.getElementById("bar-icon");
 const dropdown = document.getElementById("menu");
+
 
 barbtn.addEventListener("click", ()=>{
     dropdown.classList.toggle("show");
@@ -21,12 +18,12 @@ for(let x = 0; x < droptog.length; x++){
     droptog[x].addEventListener("click", ()=>{
         submenu[x].classList.toggle("submenushow");
     })
+}
 
 // Klik diluar sidebar untuk menghilangkan nav
-const barbtn = document.querySelector('bar-icon')
 
 document.addEventListener('click', function(e){
-    if(!barbtn.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
+    if(!barbtn.contains(e.target) && !dropdown.contains(e.target)) {
+        dropdown.classList.remove('active');
     }
-}
+})
